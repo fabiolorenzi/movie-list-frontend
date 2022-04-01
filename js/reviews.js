7936e4aaad7a099ab548addad37a2f1eaeb956e7
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-04-01 19:21:43 +0100

    developed reviews page with various functions

--- js/reviews.js
+++ js/reviews.js
@@ -1,5 +1,5 @@
 function addReview() {
-    let title = document.getElementById("titleIR").value;
+    let title = document.getElementById("titleR").value;
     let text = document.getElementById("textR").value;
     fetch("https://movie-list-backend22.herokuapp.com/api/reviews/create.php", {
         method: "POST",
@@ -13,6 +13,69 @@ function addReview() {
     })
     .then(resp => resp.json())
     .then(() => alert("Review added successfully"))
-    .then(() => location.load())
+    .then(() => location.reload())
     .catch(err => console.log(err));
+};
+
+function removeReview(id) {
+    fetch("https://movie-list-backend22.herokuapp.com/api/reviews/delete.php", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "id": id
+        })
+    })
+    .then(resp => resp.json())
+    .then(() => alert("Review deleted successfully"))
+    .then(() => location.reload())
+    .catch(err => console.log(err));
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderBody(true);
+    fetch("https://movie-list-backend22.herokuapp.com/api/reviews/read.php", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(resp => resp.json())
+    .then(data => reviews = data)
+    .then(() => renderBody(false))
+    .catch(err => console.log(err));
+});
+
+const target_id = document.getElementById("reviewsBody");
+
+var reviews = Array();
+
+function renderBody(loading) {
+    if (loading) {
+        target_id.innerHTML = 
+            `<div id="spinner">
+                <div class="spinner_container">
+                    <img src="media/spinner.png" alt="spinner" />
+                </div>
+            </div>`;
+    } else if (!loading && reviews.length === 0) {
+        target_id.innerHTML =
+            `<div id="nomovies">
+                <h1>No reviews yet</h1>
+            </div>`;
+    } else {
+        target_id.innerHTML = `<ul id="movies_list"></ul>`;
+        reviews.forEach(review => {
+            if (review.text.length > 0) {
+                document.getElementById("movies_list").innerHTML +=
+                    `<div class="review">
+                        <h1>${review.title}</h1>
+                        <h2>${review.text}</h2>
+                        <button class="removeButton" onclick="removeReview(${review.id})">Remove</button>
+                    </div>`
+                ;
+            }
+        });
+    };
 };
