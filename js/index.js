248d5714077b5ed8175e55e28660b78de547b283
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-04-01 11:26:20 +0100

    created function to fetching data

--- js/index.js
+++ js/index.js
@@ -1,17 +1,55 @@
 const target_id = document.getElementById("main_container_home");
+var movies = Array();
 
-function renderBody(status) {
-    if (status) {
+function renderBody(loading) {
+    if (loading) {
         target_id.innerHTML = 
             `<div id="spinner">
                 <div class="spinner_container">
                     <img src="media/spinner.png" alt="spinner" />
                 </div>
             </div>`;
-    }
+    } else if (!loading && movies.length === 0) {
+        target_id.innerHTML =
+            `<div id="nomovies">
+                <h1>No movies found</h1>
+            </div>`;
+    } else {
+        target_id.innerHTML = 
+            `<div id="movies_section">
+                {movies.map(movie => {
+                    return(
+                        <div class="movie" key={movie.id + "M"}>
+                            <div class="movie_img">
+                                <img src={movie.img} alt="movie_poster" />
+                            </div>
+                            <div class="movie_body">
+                                <div class="movie_data">
+                                    <h2>Title: {movie.title}</h2>
+                                    <h2>Genre: {movie.genre}</h2>
+                                    <h2>Year: {movie.rel}</h2>
+                                </div>
+                                <div class="movie_desc">
+                                    <p>{movie.descr}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>`;
+    };
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-    var isLoading = true;
-    renderBody(isLoading);
+    renderBody(true);
+    fetch("https://movie-list-backend22.herokuapp.com/api/movies/read.php", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(resp => resp.json())
+    .then(data => movies = data)
+    .then(() => renderBody(false))
+    .catch(err => console.log(err));
 });
